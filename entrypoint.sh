98f09c35981e22a88acef384423b679879559dea
Dmitrii Nabok <d.nabok@fz-juelich.de>
2024-01-25 11:23:04 +0100

    Small updates in the 'dockerized' Jupyter kernel setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ else
     connection_file=$1
 fi
 
-cat $connection_file
+# cat $connection_file
 
 # Modify the damn connection file to use proper IP address now
-sed -i 's;127.0.0.1;0.0.0.0;' ${connection_file}
+sed -i.bu 's;127.0.0.1;0.0.0.0;' ${connection_file}
 
 python3 -m ipykernel_launcher --ip=0.0.0.0 -f $connection_file
